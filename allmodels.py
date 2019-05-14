a90ace0d2c29966747a47daa6f4dab3b754bb48c
Minhao <cmhcbb@gmail.com>
2019-05-13 23:27:10 -0700

    random model added

--- allmodels.py
+++ allmodels.py
@@ -13,6 +13,9 @@ from PIL import Image
 #import pretrainedmodels.utils as utils
 import torchvision.models as models
 import torch.nn.functional as F
+from layers.feat_noise import Noise
+
+
 # Hyperparameters
 num_epochs = 50
 batch_size = 128
@@ -56,6 +59,183 @@ class IMAGENET():
         _, predict = torch.max(output.data, 1)
         return predict
 
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+class VGG_plain(nn.Module):
+    def __init__(self, vgg_name, nclass, img_width=32):
+        super(VGG_plain, self).__init__()
+        self.img_width = img_width
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, nclass)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        width = self.img_width
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                width = width // 2
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=width, stride=1)]
+        return nn.Sequential(*layers)
+
+    def predict(self, image):
+        self.eval()
+        image = torch.clamp(image,0,1)
+        image = Variable(image, volatile=True).view(1,3, 32,32)
+        if torch.cuda.is_available():
+            image = image.cuda()
+        output = self(image)[0]
+        print(output)
+        _, predict = torch.max(output.data, 1)
+        return predict[0]
+    
+    def predict_batch(self, image):
+        self.eval()
+        image = torch.clamp(image,0,1)
+        image = Variable(image, volatile=True)
+        if torch.cuda.is_available():
+            image = image.cuda()
+        output = self(image)[0]
+        _, predict = torch.max(output.data, 1)
+        return predict
+
+
+
+class VGG_rse(nn.Module):
+    def __init__(self, vgg_name, nclass, noise_init, noise_inner, img_width=32):
+        super(VGG_rse, self).__init__()
+        self.noise_init = noise_init
+        self.noise_inner = noise_inner
+        self.img_width = img_width
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, nclass)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        width = self.img_width
+        for i, x in enumerate(cfg):
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                width = width // 2
+            else:
+                if i == 0:
+                    noise_layer = Noise(self.noise_init)
+                else:
+                    noise_layer = Noise(self.noise_inner)
+                layers += [noise_layer,
+                           nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=width, stride=1)]
+        return nn.Sequential(*layers)
+
+    def predict(self, image):
+        self.eval()
+        image = torch.clamp(image,0,1)
+        image = Variable(image, volatile=True).view(1,3, 32,32)
+        if torch.cuda.is_available():
+            image = image.cuda()
+        output = self(image)[0]
+        _, predict = torch.max(output.data, 1)
+        return predict[0]
+    
+    def predict_batch(self, image):
+        self.eval()
+        image = torch.clamp(image,0,1)
+        image = Variable(image, volatile=True)
+        if torch.cuda.is_available():
+            image = image.cuda()
+        output = self(image)[0]
+        _, predict = torch.max(output.data, 1)
+        return predict
+
+
+class VGG_vi(nn.Module):
+    def __init__(self, sigma_0, N, init_s, vgg_name, nclass, img_width=32):
+        super(VGG_vi, self).__init__()
+        self.sigma_0 = sigma_0
+        self.N = N
+        self.init_s = init_s
+        self.img_width = img_width
+        self.classifier = RandLinear(sigma_0, N, init_s, 512, nclass)
+        self.features = self._make_layers(cfg[vgg_name])
+
+    def forward(self, x):
+        kl_sum = 0
+        out = x
+        for l in self.features:
+            if type(l).__name__.startswith("Rand"):
+                out, kl = l.forward(out)
+                if kl is not None:
+                    kl_sum += kl
+            else:
+                out = l.forward(out)
+        out = out.view(out.size(0), -1)
+        out, kl = self.classifier.forward(out)
+        kl_sum += kl
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        width = self.img_width
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                width = width // 2
+            else:
+                layers += [
+                        RandConv2d(self.sigma_0, self.N, self.init_s, in_channels, x, kernel_size=3, padding=1),
+                        RandBatchNorm2d(self.sigma_0, self.N, self.init_s, x),
+                        nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=width, stride=1)]
+        return nn.Sequential(*layers)
+    
+    def predict(self, image):
+        self.eval()
+        image = torch.clamp(image,0,1)
+        image = Variable(image, volatile=True).view(1,3, 32,32)
+        if torch.cuda.is_available():
+            image = image.cuda()
+        output = self(image)[0]
+        _, predict = torch.max(output.data, 1)
+        return predict[0]
+    
+    def predict_batch(self, image):
+        self.eval()
+        image = torch.clamp(image,0,1)
+        image = Variable(image, volatile=True)
+        if torch.cuda.is_available():
+            image = image.cuda()
+        output = self(image)[0]
+        _, predict = torch.max(output.data, 1)
+        return predict
 
 
 class CIFAR10(nn.Module):
@@ -262,7 +442,7 @@ def load_cifar10_data():
 
     # Data Loader (Input Pipeline)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=1, shuffle=False)
 
     return train_loader, test_loader, train_dataset, test_dataset
 
--- test_attack.py
+++ test_attack.py
@@ -7,16 +7,21 @@ from OPT_attack_lf import OPT_attack_lf
 from NES import NES
 from models import PytorchModel
 import torch
-from allmodels import MNIST, load_model, load_mnist_data, load_cifar10_data, CIFAR10
+from allmodels import MNIST, load_model, load_mnist_data, load_cifar10_data, CIFAR10, VGG_plain, VGG_rse, VGG_vi
 import os
 
 
 
 
-net = MNIST()
+#net = MNIST()
+#net.cuda()
+#net = torch.nn.DataParallel(net, device_ids=[0])
+#load_model(net,'mnist_gpu.pt')
+#net.eval()
+net = VGG_plain('VGG16', 10, img_width=32)
 net.cuda()
 net = torch.nn.DataParallel(net, device_ids=[0])
-load_model(net,'mnist_gpu.pt')
+load_model(net,'./defense_model/cifar10_vgg_plain.pth')
 net.eval()
 model = net.module if torch.cuda.is_available() else net
 
@@ -31,7 +36,8 @@ attack = OPT_attack(amodel)
 #attack = ZOO(amodel)
 
 
-train_loader, test_loader, train_dataset, test_dataset = load_mnist_data()
+#train_loader, test_loader, train_dataset, test_dataset = load_mnist_data()
+train_loader, test_loader, train_dataset, test_dataset = load_cifar10_data()
 for i, (xi,yi) in enumerate(test_loader):
     if i==50:
         break
