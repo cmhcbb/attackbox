8a52706c3a1fcaf0ca78f5ce29514ee9c9057421
Minhao <cmhcbb@gmail.com>
2019-05-16 15:07:27 -0700

    pgd attack added

---
+++ PGD.py
@@ -0,0 +1,46 @@
+from __future__ import division
+from __future__ import print_function
+
+import time
+import numpy as np
+import random
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn as nn
+class PGD(object):
+    def __init__(self,model):
+        self.model = model
+
+    def get_loss(self,xi,label_or_target,TARGETED):
+        criterion = nn.CrossEntropyLoss()
+        output = self.model.predict(xi)
+        #print(output, label_or_target)
+        loss = criterion(output, label_or_target)
+        #print(loss)
+        #print(c.size(),modifier.size())
+        return loss
+
+    def pgd(self, input_xi, label_or_target, epsilon, eta, TARGETED=False):
+        yi = Variable(label_or_target)
+        x_adv = Variable(input_xi.cuda(), requires_grad=True)
+        for it in range(1000):
+            error = self.get_loss(x_adv,yi, TARGETED)
+            if (it)%10==0:
+                print(error.data.item()) 
+            #x_adv.grad.data.zero_()
+            error.backward(retain_graph=True)
+            if TARGETED:
+                x_adv.data = x_adv.data - eta* x_adv.grad
+            else:
+                x_adv.data = x_adv.data + eta* x_adv.grad
+            diff = x_adv.data - input_xi
+            diff.clamp_(-epsilon,epsilon)
+            x_adv.data=(diff + input_xi).clamp_(0, 1)
+        return x_adv
+
+    def __call__(self, input_xi, label_or_target, epsilon, eta=0.1, TARGETED=False):
+        adv = self.pgd(input_xi, label_or_target, epsilon, eta, TARGETED)
+        return adv  
+
--- test_attack.py
+++ test_attack.py
@@ -2,9 +2,11 @@ from CW import CW
 from FGSM import FGSM
 from OPT_attack import OPT_attack
 from OPT_genattack import OPT_genattack
+from OPT_attack_sign_SGD_lf import OPT_attack_sign_SGD_lf 
 from ZOO import ZOO
 from OPT_attack_lf import OPT_attack_lf
 from NES import NES
+from PGD import PGD
 from models import PytorchModel
 import torch
 from allmodels import MNIST, load_model, load_mnist_data, load_cifar10_data, CIFAR10, VGG_plain, VGG_rse, VGG_vi
@@ -18,6 +20,7 @@ import os
 #net = torch.nn.DataParallel(net, device_ids=[0])
 #load_model(net,'mnist_gpu.pt')
 #net.eval()
+#net = VGG_rse('VGG16', 10, 0.2,0.1, img_width=32)
 net = VGG_plain('VGG16', 10, img_width=32)
 net.cuda()
 net = torch.nn.DataParallel(net, device_ids=[0])
@@ -29,17 +32,25 @@ model = net.module if torch.cuda.is_available() else net
 amodel = PytorchModel(model, bounds=[0,1], num_classes=10)
 #attack = CW(amodel)
 #attack = FGSM(amodel)
-attack = OPT_attack(amodel)
+#attack = OPT_attack(amodel)
+#attack = OPT_attack_sign_SGD_lf(amodel)
 #attack = OPT_genattack(amodel) 
 #attack = OPT_attack(amodel)  
 #attack = NES(amodel)
 #attack = ZOO(amodel)
-
+attack = PGD(amodel)
 
 #train_loader, test_loader, train_dataset, test_dataset = load_mnist_data()
 train_loader, test_loader, train_dataset, test_dataset = load_cifar10_data()
 for i, (xi,yi) in enumerate(test_loader):
-    if i==50:
+    print("image "+str(i))
+    if i==1:
+        #continue
         break
+    xi,yi = xi.cuda(), yi.cuda()
     #if i==3:
-    adv=attack(xi,yi)
+    #amodel.predict_ensemble(xi)
+    adv=attack(xi,yi, 0.05)
+    r_count= (torch.max(amodel.predict(adv),1)[1]==yi).nonzero().shape[0]
+    #total_r_count += r_count
+    print(r_count/100)
